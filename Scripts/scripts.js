d87f6ea9a35477ff3cf7c3210a5b0108317b0a19
Bernardo Mata <matabernardo2@gmail.com>
2024-03-27 19:32:10 -0600

    creacion de boton hamburguesa y responsive

---
+++ Scripts/scripts.js
@@ -0,0 +1,13 @@
+let hamburguerboton = document.getElementById("hamburguesaBoton")
+let listaNav = document.getElementsByClassName("listaNav")
+
+hamburguerboton.addEventListener("click", function () {
+    for (var i = 0; i < listaNav.length; i++) {
+        // Obtén el elemento actual
+        var elemento = listaNav[i];
+        
+        // Aplica la función deseada, por ejemplo, eliminar una clase
+        elemento.classList.toggle("max-sm:hidden");
+      }
+
+})
